b1acfa396d33f122292b9bf2a8f595c577a89a0e
matsuo.hiroshi <matsuo.hiroshi@moneyforward.co.jp>
2022-09-03 11:12:22 +0900

    init

---
+++ app.js
@@ -0,0 +1,42 @@
+function main()
+{
+  const sheet = '' // todo
+  const config = getConfig()
+
+  const jiraTickets = getJiraTickets()
+  const tickets = prepareTickets(jiraTickets)
+  fillJiraTicketsToSheet(sheet, tickets)
+}
+
+function getJiraTickets()
+{
+  const url = `https://moneyforward.atlassian.net/rest/api/3/search?maxResults=100&fields=-description,-parent,-issuelinks,-subtasks,-comment&jql=project = CAMID`
+
+  const response = UrlFetchApp.fetch(url, {
+    contentType: "application/json",
+    headers: {
+      "Authorization": "Basic " + getToken()
+    }
+  })
+
+  return JSON.parse(response)
+}
+
+function prepare(tickets)
+{
+}
+
+function fillJiraTicketsToSheet(tickets)
+{
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('JIRA Tickets')
+  sheet.clear()
+
+tickets = [[1, 2, 3], [3,4,8]]
+  sheet.getRange(2, 1, tickets.length, tickets[0].length)
+       .setValues(tickets)
+}
+
+function getConfig()
+{
+  return {}
+}
